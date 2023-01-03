3c22f8c8d40eb2f474f5cd046818874eb7539afe
vikashsah1309 <vkumarsah13@gmail.com>
2023-01-03 19:01:14 +0530

    Add files via upload

---
+++ RichText.ps1
@@ -0,0 +1,37 @@
+﻿Write-Host "Search started $(Get-Date -format 'u')"
+
+$startPath = "master:/sitecore/content/Southeastern"
+$itemsToProcess = Get-ChildItem $startPath  -Recurse
+$itemsToProcess.Count
+
+$list = [System.Collections.ArrayList]@()
+if($itemsToProcess -ne $null) {
+    $itemsToProcess | ForEach-Object { 
+        foreach($field in $_.Fields) {
+            if($field.Type -eq "Rich Text") {
+                  if($field -match "~/link.aspx") 
+                  {
+                     $info = [PSCustomObject]@{
+						"ID"=$_.ID
+						"ItemPath"=$_.ItemPath
+						"TemplateName"=$_.TemplateName
+						"FieldName"=$field.Name
+						"FieldType"=$field.Type
+						"FieldValue"=$field
+					}
+                    [void]$list.Add($info)
+                } 
+            }
+        }
+    }
+}
+  
+ 
+Write-Host "Search ended $(Get-Date -format 'u')"
+Write-Host "Items found: $($list.Count)"
+$list | Format-Table
+
+$list | Show-ListView 
+
+
+Close-Window 
