4c177046968a6d42524ea7f4c790808acd02afbd
vikashsah1309 <vkumarsah13@gmail.com>
2023-01-11 23:56:20 +0530

    #Find the list of PNG images.
    Find the types of media in Media Library. i.e. PNG, JPEG, VTT, etc.

--- List all png images.ps1
+++ List all png images.ps1
@@ -1,3 +1,34 @@
-﻿$startPath = "master:/sitecore/media library"
-$ImagePng= Get-ChildItem $startPath -Recurse | Where-Object {$_["Extension"] -eq "png"} #find the types of media, i.e. (.png, .jpeg,.vtt,etc)
+﻿#------------------- First Approch--------------------#
+$startPath = "master:/sitecore/media library"
+$ImagePng= Get-ChildItem $startPath -Recurse | Where-Object {$_["Extension"] -eq "png"} #find the types of media, i.e. (PNG, JPEG, VTT,etc)
 $ImagePng | Show-ListView
+
+#------------------- Second Approch--------------------#
+
+$startPath = "master:/sitecore/media library"
+$itemsToProcess = Get-ChildItem $startPath  -Recurse
+$itemsToProcess.Count
+
+$list = [System.Collections.ArrayList]@()
+if($itemsToProcess -ne $null) {
+    $itemsToProcess | ForEach-Object { 
+        foreach($field in $_.Fields) {
+            if($field.Name -eq "Extension") {  
+                  if($_["Extension"] -eq "png")  #find the types of media, i.e. (PNG, JPEG, VTT,etc)
+                  {
+                     $info = [PSCustomObject]@{
+						"ID"=$_.ID
+						"ItemPath"=$_.ItemPath
+						"TemplateName"=$_.TemplateName
+						"FieldValue"=$field
+					}
+                    [void]$list.Add($info)
+                } 
+            }
+        }
+    }
+}
+$list | Show-ListView 
+
+
+Close-Window 
