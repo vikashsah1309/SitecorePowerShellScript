7e33ec17de745e7c905c7e417b2536b37e3cf047
vikashsah1309 <vkumarsah13@gmail.com>
2023-01-11 23:49:00 +0530

    #find the list of PNG images.
    Find the types of media in Media Library. i.e. PNG, JPEG, VTT, etc.

---
+++ List all png images.ps1
@@ -0,0 +1,3 @@
+﻿$startPath = "master:/sitecore/media library"
+$ImagePng= Get-ChildItem $startPath -Recurse | Where-Object {$_["Extension"] -eq "png"} #find the types of media, i.e. (.png, .jpeg,.vtt,etc)
+$ImagePng | Show-ListView
